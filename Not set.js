ebf529537e7d09cf9434f3a63ed4c7f9c2ac1173
lavenderhazana <77904873+anavvanzin@users.noreply.github.com>
2026-03-02 19:07:42 -0300

    Create Not set.js

---
+++ Not set.js
@@ -0,0 +1,20 @@
+{
+	"translatorID": "58ca3819-dee9-414f-b3d8-0462844f4065",
+	"label": "Not set",
+	"creator": "",
+	"target": "",
+	"minVersion": "5.0",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2025-09-06 20:13:23"
+}
+
+
+
+/** BEGIN TEST CASES **/
+var testCases = [
+]
+/** END TEST CASES **/
